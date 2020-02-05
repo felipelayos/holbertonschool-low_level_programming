90d5bf3ebb46a28693089fbd4f9a48dafce3b535
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-02-05 21:24:08 +0000

    Task 9 fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
 * main - main function
 * Return: 0
 */
 int main(void)
 {
-perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
