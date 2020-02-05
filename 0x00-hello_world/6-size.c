dd4f2f26c402fc673e6c03a703974471619092e5
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-02-05 20:01:49 +0000

    Task 6 completed

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - main function
+* Return: 0
+*/
+int main(void)
+{
+printf("Size of a char: %zu\n", sizeof(char));
+printf("Size of an int: %zu\n", sizeof(int));
+printf("Size of a long int: %zu\n", sizeof(long));
+printf("Size of a float: %zu\n", sizeof(float));
+return (0);
+}
