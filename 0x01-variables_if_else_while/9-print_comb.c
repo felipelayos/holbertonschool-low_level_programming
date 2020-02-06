7caf187af62ba9fadf5a705dfd82fd9d784eb43f
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-02-06 20:24:22 +0000

    Fix

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - Combinations
+*Return: 0
+*/
+int main(void)
+{
+	
+
+	for (n = 0 ; n <= 9 ; n++)
+	{
+	putchar(n);
+	putchar(44);
+	putchar(32);
+	}
+	putchar('\n');
+	return (0);
+}
