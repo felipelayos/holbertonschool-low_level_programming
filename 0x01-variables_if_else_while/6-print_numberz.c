e161eee8bad48f120f153b022f81062458a738f2
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-02-06 19:05:44 +0000

    Task 6 completed

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - numbers
+*Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 48 ; n <= 57 ; n++)
+	{
+	putchar(n);
+	}
+	if (n > 57)
+	{
+	putchar('\n');
+	}
+	return (0);
+}
