1c3b7d549455dac91c3a7f4093a0db3ecc21de90
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-02-06 17:20:42 +0000

    Task 5 completed

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - numbers
+*Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n <= 9 ; n++)
+	{
+	printf("%d", n);
+	}
+	if (n == 10)
+	{
+	printf("\n");
+	}
+	return (0);
+}
