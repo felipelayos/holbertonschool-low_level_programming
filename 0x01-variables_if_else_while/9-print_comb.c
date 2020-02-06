979bc3be7dca442ff3a5447699b3cb60a93b104a
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-02-06 20:33:41 +0000

    Task 9 completed

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 /**
-*main - Combinations
+*main - numbers
 *Return: 0
 */
 int main(void)
 {
-	
+	int n;
 
-	for (n = 0 ; n <= 9 ; n++)
+	for (n = 48 ; n <= 57 ; n++)
 	{
 	putchar(n);
+	if (n != 57)
+	{
 	putchar(44);
 	putchar(32);
+	}
 	}
 	putchar('\n');
 	return (0);
