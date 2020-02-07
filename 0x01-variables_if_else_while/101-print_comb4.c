72a48e956c335bc64ffba9a73e63a2d42b529740
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-02-07 17:49:15 +0000

    Task 12 completed

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+*main - Numbers
+*Return: 0
+*/
+int main(void)
+{
+	int n, i, k;
+
+	for (k = 48 ; k <= 57 ; k++)
+	{
+	for (n = k + 1 ; n <= 57 ; n++)
+	{
+		for (i = n + 1 ; i <= 57 ; i++)
+		{	
+			putchar(k);
+			putchar(n);
+			putchar(i);
+			if (!(k == 55 && n == 56 && i == 57))
+			{
+			putchar(44);
+			putchar(32);
+			}
+		}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
