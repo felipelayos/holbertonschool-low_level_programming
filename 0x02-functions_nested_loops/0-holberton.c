ffe9af6717e40199d6ec81bfc5c25d9a9adb1fac
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-02-10 17:53:54 +0000

    Task 0 completed

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,10 +1,19 @@
-#include <stdio.h>
+#include "holberton.h"
 /**
 *main - print Holberton
 *Return: 0
 */
 int main(void)
 {
-	printf("Holberton\n");
-	return (0);
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);	
 }
