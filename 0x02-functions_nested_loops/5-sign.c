4c2ec6589fdd1fb938096e1b83f2a28cfcfb901b
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-02-11 17:15:58 +0000

    Task 5 fixed

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,28 +1,29 @@
 #include "holberton.h"
 /**
-*  int print_sign(int n);
+*  int print_sign - Function
 * Return: 1 0 and -1
+* @n: parameter
 */
 int print_sign(int n)
 {
 	int i;
 	char j;
+
 	if (n > 0)
 	{
-		i=1;
-		j='+';
+		i = 1;
+		j = '+';
 	}
-	else if(n < 0)
+	else if (n < 0)
 	{
-		i=-1;
-		j='-';
+		i = -1;
+		j = '-';
 	}
 	else
 	{
-		i=0;
-		j='0';
+		i = 0;
+		j = '0';
 	}
-	
 	_putchar(j);
-	return(i);
+	return (i);
 }
