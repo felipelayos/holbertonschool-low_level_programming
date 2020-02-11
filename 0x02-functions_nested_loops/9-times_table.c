cfc670ee440f2e914e1cbe1bd127568660d543b5
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-02-11 21:44:31 +0000

    Task 9 fixed

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,30 +10,23 @@ void times_table(void)
 
 	for (i = 0 ; i <= 9 ; i++)
 	{
-		for (j = 0 ; j <= 9 ; j++)
+		_putchar('0');
+		for (j = 1 ; j <= 9 ; j++)
 		{
-			
-		
-			if ((i*j) > 9)
+			if ((i * j) > 9)
 			{
+			_putchar(',');
+			_putchar(' ');
 			_putchar(i * j / 10 + '0');
 			_putchar(i * j % 10 + '0');
-				if(j != 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
 			}
-			else if((i*j) <= 9)
+			else if ((i * j) <= 9)
 			{
-				_putchar(i * j + '0');
-			
-				if(j != 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(i * j + '0');
+				
 			}
 		}
 		_putchar('\n');
