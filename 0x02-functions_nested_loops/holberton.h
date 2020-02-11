e139e423b85124919446c8905a6348058f8b3832
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-02-11 15:04:42 +0000

    header updated

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,6 +1,12 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
 /* _putchar function */
 
 int _putchar(char c);
 
 /*Task 1 - Alphabet*/
 void print_alphabet(void);
+
+/*Task 2 - Alphabet x10 */
+void print_alphabet_x10(void);
+#endif
