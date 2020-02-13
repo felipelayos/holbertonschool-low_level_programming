481b5a8a39bd672cf9ffd7ede961ddbd310508e4
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-02-13 14:45:03 +0000

    Task 0 completed

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+*_isupper - checks for uppercase character
+*Return: int
+*@c: parameter 
+*/
+int _isupper(int c)
+{
+	int i;
+
+	if (c >= 'a' && c <= 'z')
+	{
+		i = 0;
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		i = 1;
+	}
+	return (i);
+}
---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,7 @@
+/* Prototypes */
+
+/* _putchar function */
+int _putchar(char c);
+
+/* Task 1 - is upper */
+int _isupper(int c);
