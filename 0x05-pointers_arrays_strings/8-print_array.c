c8ae9b0340784615bba78ddce9a2e49c3a501bb9
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-02-18 21:29:23 +0000

    Task 8 completed

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+  *print_array - function
+  *@a: argument
+  *@n: argument
+  */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (i = 0 ; i <= n - 1 ; i++)
+	{
+		printf("%d", a[i]);
+		
+		if (!(i == n - 1))
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
