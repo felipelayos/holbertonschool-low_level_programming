31f349e1c60b23dde187ad83b8e142360bea9886
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-02-18 16:15:16 +0000

    Task 4 completed and trash file deleted

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+  *print_rev - function
+  *@s: parameters
+  */
+void print_rev(char *s)
+{
+	int cont = 0;
+	int i;
+
+	while (s[cont] != '\0')
+	{
+		cont += 1;
+	}
+	for (i = cont ; i >= 0 ; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
