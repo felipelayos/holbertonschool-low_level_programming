4d36e0d3e1240a5c8eb81e535926fb9e76bed8f0
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-02-18 20:41:37 +0000

    Task 7 completed

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+  *puts_half - function
+  *@str: parameters
+  */
+void puts_half(char *str)
+{
+	int i = 0, size = 0, half = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	size = i;
+
+	if (size % 2 == 0)
+	{
+		half = size / 2;
+	}else
+	{
+		half = (size / 2 ) - 1;
+	}
+
+	for (i = half ; i <= size - 1 ; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
