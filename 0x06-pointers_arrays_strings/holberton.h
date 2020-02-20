1e5c77f8916edb2b49573bb9834eb223983812de
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-02-20 16:07:36 +0000

    holberton.h header file added

---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,29 @@
+/* _putchar */
+int _putchar(char c);
+
+/* Task 0 */
+char *_strcat(char *dest, char *src);
+
+/* Task 1  */
+char *_strncat(char *dest, char *src, int n);
+
+/* Task 2  */
+char *_strncpy(char *dest, char *src, int n);
+
+/* Task 3 */
+int _strcmp(char *s1, char *s2);
+
+/* Task 4 */
+void reverse_array(int *a, int n);
+
+/* Task 5 */
+char *string_toupper(char *);
+
+/* Task 6 */
+char *cap_string(char *);
+
+/* Task 7 */
+
+
+/* Task 8 */
+char *rot13(char *);
