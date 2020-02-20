ae1d7c3e72e9900b6d342554e7821f92279b39a2
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-02-20 21:07:32 +0000

    Task 5 completed

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+  *string_toupper
+  * @*:
+  *Return: char
+  */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while(s[i] != '\0') 
+	{
+		if(s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] -= 32; 
+		}
+		i++;
+	}
+
+	return (s);
+}
