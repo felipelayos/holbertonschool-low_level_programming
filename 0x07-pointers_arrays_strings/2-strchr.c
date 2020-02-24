c36465d810fb888aa324a091e525ce3a24a45e03
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-02-24 19:13:03 +0000

    Task 2 completed

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+  *_strchr - function
+  *@s: parametro
+  *@c: parametro
+  *Return: char
+  */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+		i++;
+	}
+	return ('\0');
+}
