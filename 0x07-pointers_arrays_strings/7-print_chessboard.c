fc4be5455a3327362fed645ece7a1fcbda5641bf
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-02-24 21:24:18 +0000

    Task 6 completed

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+  *print_chessboard
+  */
+void print_chessboard(char (*a)[8])
+{
+	int i = 0, j = 0;
+
+	for (i = 0 ; i < 8 ; i++)
+	{
+		for (j = 0 ; j < 8 ; j++)
+		{
+			_putchar(a[i][j]);
+			if (j == 7)
+			{
+				_putchar('\n');
+			}
+		}
+	}
+}
