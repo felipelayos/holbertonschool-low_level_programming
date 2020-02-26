a59ff51d206e3b0cdfd3cd830b46999b09189384
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-02-26 19:19:51 +0000

    Task files created

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+  *_sqrt_recursion - function to find the natural square root of a number
+  *@n: number
+  *Return: natural square root value
+  */
+int _sqrt_recursion(int n)
+{
+	int i = 1;
+
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+  *is_prime_number
+  *@n
+  *Return: int
+  */
+int is_prime_number(int n)
+{
+	
+}
