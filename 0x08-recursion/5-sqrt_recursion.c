7943b7d18883f688efd5401921d02bf28044bd46
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-02-26 21:30:09 +0000

    Task 5 comoleted

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,26 @@
 #include "holberton.h"
 /**
+  *incremento - function to find the natural square root of a number
+  *@m: number
+  *@x: auxiliar variable
+  *Return: natural square root value
+  */
+int incremento(int m, int x)
+{
+	if ((x * x) > m)
+	{
+		return (-1);
+	}
+	else if ((x * x) != m)
+	{
+		return (incremento(m, x + 1));
+	}
+	else
+	{
+		return (x);
+	}
+}
+/**
   *_sqrt_recursion - function to find the natural square root of a number
   *@n: number
   *Return: natural square root value
@@ -7,5 +28,16 @@
 int _sqrt_recursion(int n)
 {
 	int i = 1;
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+
+	return (incremento(n, i));
 
 }
