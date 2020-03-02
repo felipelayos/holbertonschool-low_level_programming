17986facefa09f4df5a56cd3d71339a221e56dec
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-03-02 04:31:29 +0000

    Task 1 completed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: param
+ * @argv: param
+ * Return: int
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
