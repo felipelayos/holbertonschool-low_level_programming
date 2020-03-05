c1fad338f8514efbecf05cd61acb1793a51177b3
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-03-05 16:24:07 +0000

    Task 1 completed

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+*_strdup - function
+*@str: param
+*Return: char
+*/
+char *_strdup(char *str)
+{
+	char *duplicatedArray;
+	int i = 0, size = 0;
+
+	/* If the array str = NULL return NULL. */
+	if (str == '\0')
+	{
+		return ('\0');
+	}
+
+	/*Size of the array */
+	while (str[size] != '\0')
+	{
+		size++;
+	}
+	
+	/*Size of the array to duplicate */
+	duplicatedArray = malloc(sizeof(char) * size);
+
+	/* Duplicate the array on the other array*/
+	for (i = 0 ; i <= size ; i++)
+	{
+		duplicatedArray[i] = str[i];
+	}
+
+	return (duplicatedArray);
+}
