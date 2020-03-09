e73e7133cd2f7e51a75f9ff5f596736e47148820
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-03-09 20:39:09 +0000

    Task 2 completed

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+/**
+*print_dog - function
+*@d: structure
+* Description - Print the information of the dog
+*/
+#include "dog.h"
+#include <stdio.h>
+
+void print_dog(struct dog *d)
+{
+
+	if (!d)
+		return;
+
+	if ((*d).name)
+		printf("Name: %s\n", (*d).name);
+	else
+		printf("Name: (nil)");
+
+	if ((*d).age)
+		printf("Age: %f\n", (*d).age);
+	else
+		printf("Age: (nil)");
+
+	if ((*d).owner)
+		printf("Owner: %s\n", (*d).owner);
+	else
+		printf("Owner: (nil)");
+}
