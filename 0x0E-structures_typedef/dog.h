38fe123112c57cd4f27e36370529a19d087b1022
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-03-09 18:17:21 +0000

    Header and README file added

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+/**
+ * struct dog - dog
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: dogs owner
+ * Description: description
+ */
+	struct dog
+	{
+		char *name;
+		float age;
+		char *owner;
+	};
+#endif
