1997f7db7d57d610d7b9f57a025951caa25d987d
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-03-10 20:28:45 +0000

    Task 4 completed

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,43 @@
+#include <stdlib.h>
 #include "dog.h"
+/**
+*new_dog - function
+*@name: dog name
+*@age: dog age
+*@owner: dog owner
+*Description: funtion
+*Return: dog
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog
+        dog_t *dog2;
+        int i = 0;
+
+        dog2 = malloc(sizeof(struct dog));
+        while (name[i])
+                i++;
+        (*dog2).name = malloc(sizeof(char) * i + 1);
+        if ((*dog2).name == 0)
+        {
+                free(dog2);
+                return (0);
+        }
+        i = 0;
+        while (owner[i])
+                i++;
+        (*dog2).owner = malloc(sizeof(char) * i + 1);
+        if ((*dog2).owner == 0)
+        {
+                free((*dog2).name);
+                free(dog2);
+                return (0);
+        }
+        for (i = 0 ; name[i] != 0 ; i++)
+                (*dog2).name[i] = name[i];
+        (*dog2).name[i] = name[i];
+        for (i = 0 ; owner[i] != 0 ; i++)
+                (*dog2).owner[i] = owner[i];
+        (*dog2).owner[i] = owner[i];
+        (*dog2).age = age;
+        return (dog2);
 }
