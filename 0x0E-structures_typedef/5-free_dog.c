7c6f978eac4e59ead0d61bf2f408c4bbd63f36da
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-03-10 20:32:47 +0000

    Task completed

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -3,13 +3,12 @@
 *@d: parm
 *Description: func
 */
-
 void free_dog(dog_t *d)
 {
 	if (d)
 	{
 		free((*d).name);
 		free((*d).owner);
-		free(d);
+		free((*d));
 	}
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,8 +19,8 @@ typedef struct dog dog_t;
 
 dog_t *new_dog(char *name, float age, char *owner);
 
-void free_dog(dog_t *d);
 int _putchar(char c);
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
