669f48fbf288d1c25221c75556a15b6a4fd9d6eb
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-03-10 21:01:39 +0000

    Task 4 fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,33 +12,45 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog2;
 	int i;
-
+	
+	/* Asignar espacio de memoria para las copias*/
 	dog2 = malloc(sizeof(struct dog));
+	if (dog2 == NULL)
+	{
+		return (NULL);
+	}
+	/* Contar y asignar memoria para la copia del nombre */
 	for (i = 0 ; name[i] != '\0' ; i++)
 	{
 	}
 	(*dog2).name = malloc(sizeof(char) * i + 1);
-	if ((*dog2).name == 0)
+	/*Validar que no falle*/
+	if ((*dog2).name == '\0')
 	{
 		free(dog2);
-		return (0);
+		return (NULL);
 	}
+	/* Contar y asignar memoria para la copia del owner */
 	for (i = 0 ; owner[i] != '\0' ; i++)
 	{
 	}
 	(*dog2).owner = malloc(sizeof(char) * i + 1);
-	if ((*dog2).owner == 0)
+
+	/*Validar que no falle */
+	if ((*dog2).owner == '\0')
 	{
 		free((*dog2).name);
 		free(dog2);
-		return (0);
+		return (NULL);
 	}
+
+	/*Crear las copias */
 	for (i = 0 ; name[i] != '\0' ; i++)
 		(*dog2).name[i] = name[i];
-	(*dog2).name[i] = name[i];
+	(*dog2).name[i] = '\0';
 	for (i = 0 ; owner[i] != '\0' ; i++)
 		(*dog2).owner[i] = owner[i];
-	(*dog2).owner[i] = owner[i];
+	(*dog2).owner[i] = '\0';
 	(*dog2).age = age;
 	return (dog2);
 }
