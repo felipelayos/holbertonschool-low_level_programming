420e9437a7631ca15f86aeada6cac31d69e612e9
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-03-10 21:11:25 +0000

    Task 5 completed

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include "dog.h"
 /**
 *free_dog - function
 *@d: parm
@@ -5,10 +7,10 @@
 */
 void free_dog(dog_t *d)
 {
-	if (d)
+	if (d != NULL)
 	{
-		free((*d).name);
-		free((*d).owner);
-		free((*d));
+		free(d->name);
+		free(d->owner);
+		free(d);
 	}
 }
