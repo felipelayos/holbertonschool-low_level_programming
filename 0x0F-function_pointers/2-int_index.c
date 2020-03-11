03eb6f803a764af846ba40f1d0fa90d20450e0fa
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-03-11 16:19:07 +0000

    Task 2 completed

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+/**
+* int_index - function
+* @array: param
+* @size: param
+* @cmp: param
+* Return: int
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0 ; i < size ; i++)
+	{
+		if ((*cmp)(array[i]))
+			return (i);
+	}
+	return (-1);
+}
