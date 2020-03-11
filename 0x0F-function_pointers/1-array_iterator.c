1e0125d9185a1de497807684d240878e5a37d645
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-03-11 17:56:39 +0000

    Task 1 fix

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,7 +8,8 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
-	if (action)
+
+	if (action && size && array)
 	{
 		for (i = 0 ; i <= size ; i++)
 			(*action)(array[i]);
