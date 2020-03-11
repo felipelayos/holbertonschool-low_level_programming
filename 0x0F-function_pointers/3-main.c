de9f88726a285e417a0eb25caff3b0c2adafee82
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-03-11 23:36:58 +0000

    Task 0

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * main - main function
+ * @argc: param
+ * @argv: param
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	char *op;
+	int num1, num2;
+
+	/* Validar que entren 4 elements ./archivo, numero1, operacion, numero2 */
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	/* Validar que el operador sea correcto */
+	if ((argv[0] != '+' || argv[0] != '-' || argv[0] != '*' || argv[0] != '/' ||
+	argv[0] != '%') || argv[1] == '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	/* Pasar los numeros a enteros */
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	/*Validar que no se vaya a dividir o sacar modulo por 0 */
+	if ((argv[0] == '/' || argv[0] == '%') && (num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	/*Imprimir la operación segun haya ingresado el usuario*/
+	op = argv[2];
+	printf("%d\n", (*get_op_func(op))(num1, num2));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+ #include "3-calc.h"
+/**
+ *op_add - function
+ *@a: num
+ *@b: num
+ *Return: int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ *op_sub - function
+ *@a: num
+ *@b: num
+ *Return: int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ *op_mul - function
+ *@a: num
+ *@b: num
+ *Return: int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ *op_div - function
+ *@a: num
+ *@b: num
+ *Return: int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ *op_mod - function
+ *@a: num
+ *@b: num
+ *Return: int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
