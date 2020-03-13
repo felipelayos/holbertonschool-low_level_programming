3324c64d34430fa532d96a4c5426d0ab76d2bc86
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-03-13 23:20:03 +0000

    sttructure

--- 0x10-variadic_functions.c/variadic_functions.h
+++ 0x10-variadic_functions.c/variadic_functions.h
@@ -6,4 +6,9 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+typedef struct _data_type
+{
+    char *op;
+} op_dato;
+
 #endif
