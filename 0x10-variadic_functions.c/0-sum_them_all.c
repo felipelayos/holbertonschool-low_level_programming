c07a11f809bf4be9f25072b70a031c3f41ff807c
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-03-13 19:12:28 +0000

    Task 0 betty fix

--- 0x10-variadic_functions.c/0-sum_them_all.c
+++ 0x10-variadic_functions.c/0-sum_them_all.c
@@ -1,28 +1,28 @@
 #include <stdarg.h>
 /**
  * sum_them_all - function to add numbers
- * 
+ *
  * @n: number of parameters
- * 
+ *
  * Return: add of the numbers
  */
 int sum_them_all(const unsigned int n, ...)
 {
-    int sum = 0;
-    unsigned int i;
+	int sum = 0;
+	unsigned int i;
 
-    va_list list;
-    va_start(list, n);
+	va_list list;
 
-    if (n == 0)
-    {
-        return (0);
-    }
+	va_start(list, n);
 
-    for (i = 0 ; i < n ; i++)
-    {
-        sum += va_arg(list, int);
-    }
-    
-    return (sum);
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(list, int);
+	}
+	return (sum);
 }
