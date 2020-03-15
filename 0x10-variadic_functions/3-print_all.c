be23e4e878282315854ea0aa5d1a6c7e2f0106cb
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-03-15 16:26:22 +0000

    Task 3 - correction of segmentation fault

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,17 +1,14 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include "variadic_functions.h"
-
 /**
  * print_all - function to print anything
- *@format: print format.
+ * @format: format to print
  */
 void print_all(const char *const format, ...)
 {
 	int i, j;
 	va_list valist;
-	char *separator;
-
 	print_type ops[] = {
 		{"c", p_char},
 		{"i", p_int},
@@ -19,11 +16,12 @@ void print_all(const char *const format, ...)
 		{"s", p_string},
 		{0, 0}};
 
+	char *separator;
 
-	va_start(valist, format);
-	i = 0;
 	separator = "";
 
+	i = 0;
+	va_start(valist, format);
 	while (format != 0 && format[i] != 0)
 	{
 		j = 0;
@@ -32,28 +30,28 @@ void print_all(const char *const format, ...)
 			if (ops[j].p[0] == format[i])
 			{
 				printf("%s", separator);
-				separator = ", ";
 				ops[j].f(valist);
+				separator = ", ";
 			}
 			j++;
 		}
 		i++;
 	}
-	va_end(valist);
 	printf("\n");
+	va_end(valist);
 }
 
 /**
  * p_char - function to print a char
- * @valist: list of arguments
+ * @argument: Holds the va_list
  */
 void p_char(va_list argument)
 {
-	printf("%s", va_arg(argument, char *));
+	printf("%c", va_arg(argument, int));
 }
 /**
  * p_int - function to print an int
- * @valist: list of arguments
+ * @argument: Holds the va_list
  */
 void p_int(va_list argument)
 {
@@ -61,7 +59,7 @@ void p_int(va_list argument)
 }
 /**
  * p_float - function to print a float
- * @valist: list of arguments
+ * @argument: Holds the va_list
  */
 void p_float(va_list argument)
 {
@@ -69,13 +67,13 @@ void p_float(va_list argument)
 }
 /**
  * p_string - function to print a string
- * @valist: list of arguments
+ * @argument: Holds the va_list
  */
 void p_string(va_list argument)
 {
 	char *ptr_aux;
-	ptr_aux = va_arg(argument, char *);
 
+	ptr_aux = va_arg(argument, char *);
 	if (ptr_aux == 0)
 	{
 		ptr_aux = "(nil)";
