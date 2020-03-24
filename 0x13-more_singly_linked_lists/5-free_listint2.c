d200a9a9021f8bb7d58951209d48bcb1cf051147
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-03-24 15:48:57 +0000

    free a list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - free a list
+ * @head: list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *node;
+
+	if (*head != 0)
+	{
+		while (*head != 0)
+		{
+			node = *head;
+			*head = (*head)->next;
+			free(node);
+		}
+	}
+}
