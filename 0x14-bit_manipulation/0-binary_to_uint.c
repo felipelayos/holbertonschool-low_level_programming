23dac2a20fea7310d83d9580b959b0ce805d2568
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-03-26 19:26:27 +0000

    prototypes

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * binary_to_uint - convert binary to int
+ * @b: pointing to a string of 0 and 1 chars
+ * Return: sum
+ */
+unsigned int binary_to_uint(const char *b)
+{
+
+	unsigned int i = 0, acum = 0;
+
+	if (b)
+	{
+		while (b[i])
+		{
+			if (b[i] != '1' && b[i] != '0')
+				return (0);
+
+			acum <<= 1;
+
+			if (b[i] == '1')
+				acum = acum + 1;
+
+			i++;
+		}
+		return (acum);
+	}
+	return (0);
+}
