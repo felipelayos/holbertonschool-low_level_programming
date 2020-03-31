647816ce662b13e8a65bd3dda87d0a3d0b96cb67
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-03-31 17:47:57 +0000

    memory free corrections

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,18 +12,23 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, n_characters, n_c_print;
-	char *buffer = malloc(sizeof(*buffer));
+	int fd, n_characters;
+	char *buffer;
+
+	buffer = malloc(letters);
 
 	if (!buffer)
 		return (0);
 
 	if (!filename)
+	{
+		free(buffer);
 		return (0);
+	}
 
-	fd = open(filename, O_RDONLY, letters);
+	fd = open(filename, O_RDONLY);
 
-	if (fd < 0) /* if open fails return 0 */
+	if (fd == -1) /* if open fails return 0 */
 	{
 		close(fd);
 		free(buffer);
@@ -32,14 +37,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	n_characters = read(fd, buffer, letters);
 
-	n_c_print = write(STDOUT_FILENO, buffer, letters);
-
-	if (n_c_print < 0) /* If write fails return 0 */
-	{
-		close(fd);
-		free(buffer);
-		return (0);
-	}
+	write(STDOUT_FILENO, buffer, letters);
 
 	close(fd);
 	free(buffer);
