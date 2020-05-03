8547795c41e7f8106bc16198e352e8b160b1e472
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-05-03 18:12:01 +0000

    count nodes

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+
+#include "lists.h"
+
+/**
+ * dlistint_len - count the number 
+ * @h: const dlistint_t
+ * Return: size_t
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
