7f5f00ceffe8cbd4e8a34c1e58224b136e5f56be
Felipe Layos <jfelipelayos@gmail.com>
2020-05-03 22:39:28 +0000

    betty fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,12 +15,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
-
 	if (idx == 0)
 	{
 		return (add_dnodeint(h, n));
 	}
-
 	node_aux = *h;
 	while (i < idx - 1)
 	{
@@ -35,7 +33,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		return (add_dnodeint_end(h, n));
 	}
-
 	else
 	{
 		node = malloc(sizeof(dlistint_t));
@@ -44,7 +41,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		{
 			return (NULL);
 		}
-
 		node->n = n;
 		node->next = node_aux->next;
 		node->next->prev = node;
