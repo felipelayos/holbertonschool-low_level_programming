e1060ea218d74f44f6ae39a45975c3fba2e6b884
Felipe Layos <jfelipelayos@gmail.com>
2020-05-28 13:58:47 -0500

    Betty fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,24 +2,24 @@
 
 /**
  * hash_table_create - Create a hash table
- *
+ * @size: size
  * Return: hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *table;
+	hash_table_t *table;
 
-    if (!size)
-        return (NULL);
+	if (!size)
+		return (NULL);
 
-    table = malloc(sizeof(hash_table_t));
-    if (!table)
-        return (NULL);
+	table = malloc(sizeof(hash_table_t));
+	if (!table)
+		return (NULL);
 
-    table[0].array = calloc(size, sizeof(char *));
-    if (!table[0].array)
-        return (NULL);
+	table[0].array = calloc(size, sizeof(char *));
+	if (!table[0].array)
+		return (NULL);
 
-    table[0].size = size;
-    return (table);
+	table[0].size = size;
+	return (table);
 }
